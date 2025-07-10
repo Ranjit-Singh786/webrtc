bc1f9ef28594332763b9aa22ea7127aab70f1d77
ranjit <ranjit94cf@gmail.com>
2025-07-10 17:54:45 +0530

    web socket

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const { Server } = require("socket.io");
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
 
-const io = new Server(8000, {
+const io = new Server(server, {
   cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-      credentials: false,
+      origin: "*"
     }
 });
 
